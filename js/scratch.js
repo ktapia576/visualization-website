19e509ae3f005c28c4f59c5df4a2f6bc3f982e19
ktapia576 <ktapia576@hotmail.com>
2019-11-10 02:09:45 -0500

    implement mock footable table and remove google charts table. Add loadfile function

--- js/scratch.js
+++ js/scratch.js
@@ -1,10 +1,15 @@
+//----------- Load Google Charts ---------------
+google.charts.load('current', {packages: ['corechart', 'line']}); // Load for Line
+google.charts.load('current', {packages: ['corechart', 'bar']});  // Load for Bar
+google.charts.load('current', {'packages':['corechart']});  // Load for Pie
+//----------- End of Loading Google Charts --------
+
 //----------- Global Variables (Data) ----------
 var csvFile = null;
 var data = null;
 var pieChart = null;
 var barChart = null;
 var lineChart = null;
-var table = null;
 //----------- End of Global Variables -----------
 
 //----------- Button Handlers -----------------
@@ -44,6 +49,36 @@ const showUserInfo = () => {
     $('#userInfoModal').modal('toggle');  // Show modal
 }
 
+//----- Load File -------
+$("#file").change( e => {  // when value of input changes (once user uploads file), do this [change event is sent to an element when its value changes]
+    loadFile();
+});
+
+const loadFile = () => { 
+    var headers;
+    csvFile = document.getElementById("file").files[0];   // get first file only
+    console.log(csvFile.size);
+      
+    // Parse local CSV file
+    Papa.parse(csvFile, {
+        delimiter: ",",
+        header: true,
+        skipEmptyLines: true, //  lines that are completely empty will be skipped
+        complete: results => {  // Callback to execute when parsing complete
+            console.log("Finished:", results); 
+            data = results.data;
+            headers = results.meta .fields;
+            //headers = data.shift(); // returns first row, which are headers, and then removes it from array
+            console.log("Headers:", headers); 
+            console.log("Data:", data);  // Get a record: data[0].Zipcode | where data[n'th item]."header"
+        }, 
+        error: error => {   // Callback to execute if FileReader encounters an error.
+            console.log(error.message); 
+        } 
+    });
+}
+//------ End of Load File ----------
+
 const exit = () => {
     // Remove all cookies
     Cookies.remove("name"); 
@@ -64,19 +99,30 @@ const exit = () => {
         lineChart.clearChart();
     }
 
-    if (table != null){
-        table.clearChart();
-    }
-
     // Clear Data
     csvFile = null;
     data = null;
     pieChart = null;
     barChart = null;
     lineChart = null;
-    table = null;
 
     // Clear Messages
     document.getElementById('messageArea').textContent = "Data has been cleared!";
 }
 //-------- End of Button Handlers ---------
+$('.table').footable({
+    "columns": [
+        { "name": "id", "title": "ID" },
+        { "name": "firstName"},
+        { "name": "lastName"},
+        { "name": "jobTitle"},
+        { "name": "started"},
+        { "name": "dob"}
+    ],
+    "rows": [
+        { "id": "123", "firstName": "John", "lastName": "Paul", "jobTitle": "Chef", "started": "11-5-20", "dob": "02-9-98" },
+        { "id": "123", "firstName": "John", "lastName": "Paul", "jobTitle": "Chef", "started": "11-5-20", "dob": "02-9-98" },
+        { "id": "123", "firstName": "John", "lastName": "Paul", "jobTitle": "Chef", "started": "11-5-20", "dob": "02-9-98" },
+        { "id": "123", "firstName": "John", "lastName": "Paul", "jobTitle": "Chef", "started": "11-5-20", "dob": "02-9-98" }
+    ]
+});
