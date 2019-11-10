d0760dcb1d632394f6d1cbad520722f8a3c2f6a5
ktapia576 <ktapia576@hotmail.com>
2019-11-09 19:03:37 -0500

    Add global vars and user info modal

--- js/scratch.js
+++ js/scratch.js
@@ -1,4 +1,13 @@
-// Button Handlers
+//----------- Global Variables (Data) ----------
+var csvFile = null;
+var data = null;
+var pieChart = null;
+var barChart = null;
+var lineChart = null;
+var table = null;
+//----------- End of Global Variables -----------
+
+//----------- Button Handlers -----------------
 const showClientInfo = () => {
     var browser = navigator.userAgent;  // Get user's browser
     var os = navigator.platform; 
@@ -15,6 +24,24 @@ const showClientInfo = () => {
     
     document.getElementById('clientInfo').innerHTML = clientInfo; // innerHTML returns HTML, textContent has better performance because its value is not parsed as HTML
     $('#clientInfoModal').modal('toggle');
+}
+
+const showUserInfo = () => {
+    var cookies = Cookies.get(); // get object of all cookies
+
+    // Check if Cookie set
+    if (cookies.username == null){ // Check if null and undefined simultaneously
+        document.getElementById('userInfo').textContent = "Login to see User info!";
+    } else {
+        var userInfo = `<b>UID:</b> ${cookies.uid}</br>
+        <b>Username:</b> ${cookies.username}</br>
+        <b>Name:</b> ${cookies.name}</br>
+        <b>Gender:</b> ${cookies.gender}</br>`;
+
+        document.getElementById('userInfo').innerHTML = userInfo; // innerHTML returns HTML, textContent has better performance because its value is not parsed as HTML
+    }
+
+    $('#userInfoModal').modal('toggle');  // Show modal
 }
 
 const exit = () => {
@@ -50,4 +77,6 @@ const exit = () => {
     table = null;
 
     // Clear Messages
+    document.getElementById('messageArea').textContent = "Data has been cleared!";
 }
+//-------- End of Button Handlers ---------
