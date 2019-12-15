7159316eac27203a8102a51aad956da08fbac3e3
ktapia576 <ktapia576@hotmail.com>
2019-12-15 16:55:40 -0500

    Add savesetting or update setting to db

---
+++ src/saveSettings.php
@@ -0,0 +1,38 @@
+<?php 
+    function saveData() {
+        include 'dbconfig.php';
+
+        $uid = $_POST["uid"];
+        $login = $_POST["login"];
+        $AvgWages = $_POST["AvgWages"];
+        $EstimatedPopulation = $_POST["EstimatedPopulation"];
+
+        $conn = db_connect();  // Connect to Database
+
+        $sql = "SELECT * FROM 2019F_tapiake.User_Setting WHERE uid='$uid'";
+
+        $result = $conn->query($sql) or die($conn->error);
+
+
+        if ($result->num_rows == 1) {
+            $sql = "UPDATE 2019F_tapiake.User_Setting SET AvgWages='$AvgWages', EstimatedPopulation='$EstimatedPopulation', datetime=NOW() WHERE uid='$uid'";
+            
+            $result = $conn->query($sql) or die($conn->error);
+
+            header('Content-Type: application/json');
+            echo json_encode(array('message' => 'Updated setting success.'));
+        } elseif ($result->num_rows == 0) {
+            $sql = "INSERT INTO 2019F_tapiake.User_Setting (uid, login, AvgWages, EstimatedPopulation, datetime) VALUES ('$uid', '".$login."','$AvgWages','$EstimatedPopulation', NOW())";
+
+            $result = $conn->query($sql) or die($conn->error);
+
+            header('Content-Type: application/json');
+            echo json_encode(array('message' => 'Saved setting success.'));
+        } else {
+            header('Content-Type: application/json');
+            echo json_encode(array('message' => 'Could not save setting'));
+        }
+    }
+    
+    saveData();
+?>
